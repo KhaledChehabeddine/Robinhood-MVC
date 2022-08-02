610eb22eb8db856c270401543576a82864f82fde
JamilShoujah <73319605+JamilShoujah@users.noreply.github.com>
2022-08-02 22:05:37 +0300

    instructor

---
+++ robinhood-mvc/Controllers/InstructorController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using robinhood_mvc.Models;
+
+namespace robinhood_mvc.Controllers;
+
+public class InstructorController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public InstructorController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ robinhood-mvc/Models/IntructorModel.cs
@@ -0,0 +1,10 @@
+namespace robinhood_mvc.Models;
+
+public class Instructor
+{
+
+    public string? InstructorName { get; set; }
+    public int Id { get; set; }
+    public string? courseName { get; set; }
+    public int rating { get; set; }
+}
